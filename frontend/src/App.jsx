dc938da98152b12f41f6febf9fa7ce02db6a5c3d
Ankita Mishra <ankitamishra8763@gmail.com>
2026-07-01 14:40:34 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Blogs from './pages/Blogs'
 import Contact from './pages/Contact'
+import Franchise from './pages/Franchise'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blogs />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/franchise" element={<Franchise />}/>
       </Routes>
       <Footer />
     </>
