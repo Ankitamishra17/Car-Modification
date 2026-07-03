6a50d9c4a98879f0763ae7576f5e14dee612e087
Ankita Mishra <ankitamishra8763@gmail.com>
2026-07-03 16:51:30 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-// import TopBar from './components/layouts/TopBar'
+
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
 import Home from "./pages/Home";
@@ -8,14 +8,13 @@ import About from "./pages/About";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import Franchise from "./pages/Franchise";
-// import Services_1_2 from "./pages/services/Sercice_1_2";
-// import Services_3_4 from "./pages/services/Service_3_4";
+
 import ServicePage from "./pages/services/ServicePage";
 
 function App() {
   return (
     <>
-      {/* <TopBar /> */}
+     
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,14 +22,8 @@ function App() {
         <Route path="/blog" element={<Blogs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/franchise" element={<Franchise />} />
-        {/* <Route
-          path="/services/ppf-ceramic-coating"
-          element={<Services_1_2 />}
-        />
-        <Route
-          path="/services/refurbish-vehicle-restore"
-          element={<Services_3_4 />}
-        /> */}
+       
+      
         <Route path="/services/:slug" element={<ServicePage />} />
       </Routes>
       <Footer />
